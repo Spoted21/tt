75e2d1a1e1fa330f8defadaea168a7c0bc1605e9
Todd Connelly <Spoted21@users.noreply.github.com>
2017-11-30 23:05:19 -0700

    Update notin.R

--- R/notin.R
+++ R/notin.R
@@ -1,1 +1,10 @@
+#' A Not In Function
+#'
+#' This function is the opposite of the %in% function.
+#'
+#' @param x a character string
+#'
+#' @keywords notin
+#' @export
+
 %notin%' <- function(x, table) match(x, table, nomatch = 0) == 0
